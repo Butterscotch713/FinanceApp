97f227193d6201e4b80dcb6800a42513227cdf00
eabock <eabock29@gmail.com>
2023-11-14 11:24:48 -0500

    buttons that do things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+declare function ClickEvent(): void;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Finance';
+   getName(){
+    alert("this is not implemented yet!")
+  }
+
+
 }
