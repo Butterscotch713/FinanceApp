966eae27b798caab08668b075c565bae57b5da54
eabock <eabock29@gmail.com>
2023-11-28 13:18:04 -0500

    Added a component for the first game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import {RouterLink} from "@angular/router";
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
+import { Game1Component } from './game1/game1.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    Game1Component,
   ],
     imports: [
         BrowserModule,
